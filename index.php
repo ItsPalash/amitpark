004c2fd89d3c1ef565a2a63662aef553d14d7b14
ItsPalash <84432951+ItsPalash@users.noreply.github.com>
2021-07-14 20:27:11 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
+<script>
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+    document.getElementById("navf").style.top = "0px";
+  } else {
+    document.getElementById("navf").style.top = "-81px";
+  }
+}
+</script>
+
+
+	  <link rel="stylesheet" href="index.css">
+	<title>Hotel Amit Park International | Home</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Marck+Script&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Uchen&display=swap" rel="stylesheet">
+</head>
+<body>
+<div id="navf" style="position:fixed; top:-81px; left:0px; background:white; height:80px; z-index:3; width:100%; border-bottom:1px solid #cccccc;">
+	<div id="navfd"><span style="font-family: 'Marck Script', cursive; border-bottom:1px solid white; font-size:27px;">AmitPark</span></div>
+	<div id="navfd" style="margin-left:100px;"><span style="border-bottom:1px solid black;">HOME</span></div>
+    <div id="navfd">ROOMS</div>
+    <div id="navfd">ABOUT</div>
+    <div id="navfd" style="margin-left:37px;">DINE</div>
+    <div id="navfd" style="margin-left:35px;">BAR</div>
+    <div id="navfd" style="margin-left:30px;">CONTACT</div>
+</div>
+
+	<div style="position:absolute; top:0; left:0; width:100%; height:105%; overflow:hidden;">
+		<img src="images/i3.webp" style="position:fixed; top:-150px; left:0%; width:100%; z-index:0;">
+
+<div id="nav1">
+	<div id="navd"><span style="border-bottom:1px solid white;">HOME</span></div>
+    <div id="navd">ROOMS</div>
+    <div id="navd">ABOUT</div>
+</div>
+<div id="nav2">
+	<div id="navd">DINE</div>
+    <div id="navd">BAR</div>
+    <div id="navd">CONTACT</div>
+</div>
+
+<p style="font-family: 'Marck Script', cursive; position:absolute; top:-32px; left:43.5%; color:white; font-size:white; font-size:47px;">AmitPark</p>
+<p style=" position:absolute; top:-65px; left:39.5%; color:white; font-size:white; font-size:65px;">H <span style="font-family: 'Marck Script', cursive; font-size:52px; position:absolute; left:-55%; top:16.1%;">A</span></p>
+	</div>
+<div style="position:absolute; top:78.2%; left:0%; width:100%; height:22.5%; background:white; z-index:2; border-top-left-radius:7px; border-bottom-left-radius:2px; font-family: 'Poppins', sans-serif;-webkit-box-shadow: 0px 12px 33px -30px rgba(0,0,0,0.35); 
+box-shadow: 0px 12px 33px -30px rgba(0,0,0,0.35);">
+
+	<div style="margin-left:120px; float:left; width:200px; height:100%; letter-spacing:2px;">
+		<span style="position:relative; top:32px; left:5px;"">CHECK-IN:</span><br>
+		<span style="position:relative; top:33px; left:10px;"><input type="date" id="birthday" name="birthday" style="font-family: 'Marck Script', cursive;border:1px solid #d9d9d9; padding:15px;"></span>
+	</div>
+
+	<div style="margin-left:0px; float:left; width:200px; height:100%; letter-spacing:2px;">
+		<span style="position:relative; top:32px; left:5px;"">CHECK-OUT:</span><br>
+		<span style="position:relative; top:33px; left:10px;"><input type="date" id="birthday" name="birthday" style="font-family: 'Marck Script', cursive;border:1px solid #d9d9d9; padding:15px;"></span>
+	</div>
+
+	<div style="margin-left:0px; float:left; width:180px; height:100%; letter-spacing:2px;">
+		<span style="position:relative; top:32px; left:5px;"">ROOMS:</span><br>
+		<span style="position:relative; top:33px; left:10px; ">
+			  <select style=" -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    padding-top: 17px; padding-right:120px; padding-left:20px; border:1px solid #d9d9d9; padding-bottom:18px; color:#595959;">
+    <option value="1">1</option>
+    <option value="2">2</option>
+  </select>
+		</span>
+	</div>
+
+	<div style="margin-left:0px; float:left; width:180px; height:100%; letter-spacing:2px;">
+		<span style="position:relative; top:32px; left:5px;"">ADULTS:</span><br>
+		<span style="position:relative; top:33px; left:10px; ">
+			  <select style=" -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    padding-top: 17px; padding-right:120px; padding-left:20px; border:1px solid #d9d9d9; padding-bottom:18px; color:#595959;">
+    <option value="1">1</option>
+    <option value="2">2</option>
+  </select>
+		</span>
+	</div>
+
+	<div style="margin-left:0px; float:left; width:180px; height:150%; letter-spacing:2px;">
+		<span style="position:relative; top:32px; left:5px;"">CHILDREN:</span><br>
+		<span style="position:relative; top:33px; left:10px; ">
+			  <select style=" -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    padding-top: 17px; padding-right:120px; padding-left:20px; border:1px solid #d9d9d9; padding-bottom:18px; color:#595959;">
+    <option value="1">1</option>
+    <option value="2">2</option>
+  </select>
+		</span>
+	</div>
+
+    <div style="margin-left:0px; float:left; width:180px; height:100%; letter-spacing:1.5px;">
+		<span style="position:relative; top:32px; left:5px;""></span><br>
+		<span style="position:relative; top:33px; left:10px;">
+<button style="background-color: #252726; letter-spacing:2px;
+  border: none;
+  color: white;
+  padding: 19px 50px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 10px;
+
+  cursor: pointer;">BOOK</button>
+		</span>
+	</div>
+
+</div>
+<link href="https://fonts.googleapis.com/css2?family=Prata&display=swap" rel="stylesheet">
+<div style="position:absolute; top:100%; left:0; width:100%; height:105%; overflow:hidden; background:white;">
+	<div style="position:absolute; top:0%; left:0; width:40%; height:100%; text-align:center;">
+		<br><br><br><br><br><br><br><br>
+		<p style="font-family: 'Poppins', sans-serif; font-size:15px; margin-bottom:-10px; margin-right:20px;">ROOMS AND SUITS</p>
+		<p style="font-family: 'Prata', serif; font-size:33px; font-weight:10; color:#191919;"><b>The Art of meeting</b> your<br> highest expectations</p>
+		<p style="font-family: 'Prata', serif; font-size:15px; width:90%; padding-left:30px; color:#4d4d4d;">Stay in our comfy & cozy rooms oozing with luxury as you enjoy a splendid variety of products through a swift room service. With a highly experienced staff available right at you fingerprints, we are sure to enhance your stay.</p>
+	</div>
+	<div style="position:absolute; top:0%; left:40%; width:60%; height:100%;">
+		<img src="images/ir1.webp" style="position:absolute; width:100%; top:0%;">
+	</div>
+
+</div>
+
+<div style="position:absolute; top:200%; left:0; width:100%; height:105%; overflow:hidden; background:white;">
+	<div style="position:absolute; top:0%; left:0; width:40%; height:100%;">
+<img src="images/ir6.webp" style="position:absolute; width:100%; top:0%;">
+	</div>
+	<div style="position:absolute; top:0%; left:40%; width:60%; height:100%;">
+		<img src="images/ir9.webp" style="position:absolute; top:10%; left:5%; width:400px;">
+	</div>
+
+</div>
+<div style="position:absolute; top:300%; left:0; width:100%; height:40%; overflow:hidden; background:white; text-align:center;">
+	<p style="font-family: 'Poppins', sans-serif;">Spa & Massage</p>
+	<p style="font-family: 'Poppins', sans-serif; color: #191919; font-size:20px;">Relax your soul and body through a guided mind-numbing pleasant experience kar</p><br>
+    <p style="font-family: 'Poppins', sans-serif; color: black; font-size:17px;"><b><span style="border-bottom:2px solid black; letter-spacing:2px;">EXLORE</span></b></p>
+</div>
+<div style="position:absolute; top:340%; left:0; width:100%; height:105%; overflow:hidden; background:white;">
+	<div style="position:absolute; top:0%; left:0; width:40%; height:100%; text-align:center;">
+		<br><br><br><br><br><br><br><br>
+		<p style="font-family: 'Poppins', sans-serif; font-size:15px; margin-bottom:-10px; margin-right:20px;">DINE AND ORDER</p>
+		<p style="font-family: 'Prata', serif; font-size:27px; font-weight:10; color:#191919;"><b>Treat yourself to a delicious</b> adventure<br> with a diverse taste palate</p>
+		<p style="font-family: 'Prata', serif; font-size:15px; width:95%; padding-left:20px; color:#4d4d4d;">From a wide variety of delicacies from all over the world we bring you the best possible dining experience right to your plate. With a highly experienced team of chefs who go the extra mile to make sure that your fresh meal is always a memorable one.</p>
+	</div>
+	<div style="position:absolute; top:0%; left:40%; width:60%; height:100%;">
+		<img src="images/if3.webp" style="position:absolute; width:100%; top:-45%;">
+	</div>
+
+</div>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+
+</body>
+</html>
